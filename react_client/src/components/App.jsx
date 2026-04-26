ed0a754a7e004e13e72fc9f7c58b45cef2c308da
Michael-Chillemi <michaelchillemi17@gmail.com>
2026-04-25 21:30:16 -0400

    making better app.js and Home page

--- react_client/src/components/App.jsx
+++ react_client/src/components/App.jsx
@@ -1,30 +1,53 @@
-//import { useState } from 'react'
-import './App.css'
-import Home from "./Home"
-//import Albums from "./Albums"
-import Artist from "./Artist"
-//import Listeners from "./Listeners"
-
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Artists from "./Artists";
+import ArtistDetail from "./ArtistDetail";
+import Albums from "./Albums";
+import AlbumDetail from "./AlbumDetail";
+import Listeners from "./Listeners";
+import ListenerDetail from "./ListenerDetail";
 
 function App() {
- // const [books, setBooks] = useState();
-
-/*
-
- <Albums />
-      
-      <Listeners />
-
-*/
-
-
-
   return (
-    <div>
-      <Home />
-      <Artist />
+    <div className="app-shell">
+      <header className="app-header">
+        <Link to="/" className="brand">
+          Chillemi-Lab7
+        </Link>
+        <nav className="app-nav">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/artists">Artists</NavLink>
+          <NavLink to="/albums">Albums</NavLink>
+          <NavLink to="/listeners">Listeners</NavLink>
+        </nav>
+      </header>
+
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/artists/:id" element={<ArtistDetail />} />
+          <Route path="/albums" element={<Albums />} />
+          <Route path="/albums/:id" element={<AlbumDetail />} />
+          <Route path="/listeners" element={<Listeners />} />
+          <Route path="/listeners/:id" element={<ListenerDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+function NotFound() {
+  return (
+    <div className="card">
+      <h2>404 - Page Not Found</h2>
+      <p>The page you requested does not exist.</p>
+    </div>
+  );
+}
+
+export default App;
--- react_client/src/components/Home.jsx
+++ react_client/src/components/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div> 
-            <h1>HOME PAGE HEADER1</h1>
-            <p>This is the home page!!!</p>
-        </div>
-    );
+  return (
+    <div className="card">
+      <h1>Lab 7</h1>
+      <p>
+        A small GraphQL + Apollo + React playground for managing artists,
+        albums, and listeners.
+      </p>
+      <div className="actions">
+        <Link to="/artists">
+          <button className="primary">Browse Artists</button>
+        </Link>
+        <Link to="/albums">
+          <button className="primary">Browse Albums</button>
+        </Link>
+        <Link to="/listeners">
+          <button className="primary">Browse Listeners</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
